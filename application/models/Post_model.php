a1e95c924d13aa3516bb3b97ad546ea4677cbacd
Roberto Arciniegas <rarciniegas@hotmail.com>
2019-11-02 11:31:25 -0400

    Pagination added

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -4,7 +4,10 @@
       $this->load->database();
     }
 
-    public function get_posts($slug = FALSE){
+    public function get_posts($slug = FALSE, $limit = FALSE, $offset = FALSE){
+      if($limit){
+				$this->db->limit($limit, $offset);
+			}
       if($slug == FALSE){
         $this->db->order_by('post.id','DESC');
         $this->db->join('category', 'category.id = post.category_id');
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -14,4 +14,8 @@
 		<p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
 		</div>
 	</div>
+
 <?php endforeach; ?>
+<div class="pagination-links">
+		<?php echo $this->pagination->create_links(); ?>
+</div>
